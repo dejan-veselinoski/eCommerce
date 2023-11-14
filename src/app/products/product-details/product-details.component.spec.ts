45b07b16cdd276ba87d782fcf019d897fb1ba9ab
Dejan <veselinoski94@gmail.com>
2023-11-14 20:00:41 +0100

    unit tests

--- src/app/products/product-details/product-details.component.spec.ts
+++ src/app/products/product-details/product-details.component.spec.ts
@@ -1,15 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
 import { ProductDetailsComponent } from './product-details.component';
+import { ActivatedRoute } from '@angular/router';
 
 describe('ProductDetailsComponent', () => {
   let component: ProductDetailsComponent;
   let fixture: ComponentFixture<ProductDetailsComponent>;
+  let mockDataService: any;
+  let mockStateService: any;
+  let mockActiveService: any;
+  let mockActivatedRoute: any;
 
   beforeEach(() => {
+    mockDataService = jasmine.createSpyObj(['query', 'mutate']);
+    mockStateService = jasmine.createSpyObj(['select', 'setState']);
+    mockActiveService = jasmine.createSpyObj(['activeOrder$']);
+    mockActivatedRoute = {
+      paramMap: of({
+        get: jasmine.createSpy().and.returnValue('test-slug')
+      })
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ProductDetailsComponent]
+      declarations: [ProductDetailsComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute }
+      ]
     });
+
     fixture = TestBed.createComponent(ProductDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +35,44 @@ describe('ProductDetailsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('subscribeToProductData', () => {
+    it('should subscribe to product data', () => {
+      const mockProductData = { product: { } };
+      mockDataService.query.and.returnValue(of(mockProductData));
+      mockStateService.select.and.returnValue(of('last-collection-slug'));
+
+      component.subscribeToProductData();
+
+      expect(component.product).toEqual(mockProductData.product);
+    });
+  });
+
+  describe('addToCart', () => {
+    it('should add the product variant to the cart', () => {
+      const mockVariant : any= { id: 'variant-id' };
+      const mockQuantity = 2;
+      const mockAddToCartResponse = { addItemToOrder: { __typename: 'Order', id: 'order-id' } };
+      mockDataService.mutate.and.returnValue(of(mockAddToCartResponse));
+
+      component.addToCart(mockVariant, mockQuantity);
+
+      expect(mockStateService.setState).toHaveBeenCalledWith('activeOrderId', 'order-id');
+    });
+  });
+
+  describe('getMostRelevantCollection', () => {
+    it('should return the most relevant collection', () => {
+      const mockCollections: any = [
+        { slug: 'collection-1', breadcrumbs: ['breadcrumb-1'] },
+        { slug: 'collection-2', breadcrumbs: ['breadcrumb-2', 'breadcrumb-3'] }
+      ];
+      const mockLastCollectionSlug = 'collection-1';
+
+      const result = component.getMostRelevantCollection(mockCollections, mockLastCollectionSlug);
+
+      expect(result).toEqual(mockCollections[0]);
+    });
   });
 });
--- src/app/products/product-list/product-list.component.spec.ts
+++ src/app/products/product-list/product-list.component.spec.ts
@@ -1,15 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { BehaviorSubject, of } from 'rxjs';
 import { ProductListComponent } from './product-list.component';
+import { ApolloTestingModule } from 'apollo-angular/testing';
+import { Apollo } from 'apollo-angular';
 
 describe('ProductListComponent', () => {
   let component: ProductListComponent;
   let fixture: ComponentFixture<ProductListComponent>;
+  let mockDataService: any;
+  let mockStateService: any;
+  let mockRouter: any;
+  let mockActivatedRoute: any;
+  let mockStore: any;
 
   beforeEach(() => {
+    mockDataService = jasmine.createSpyObj(['query']);
+    mockStateService = jasmine.createSpyObj(['loading', 'setState']);
+    mockRouter = jasmine.createSpyObj(['navigate']);
+    mockActivatedRoute = {
+      paramMap: of({
+        get: jasmine.createSpy().and.returnValue('test-slug')
+      }),
+      queryParamMap: of({
+        get: jasmine.createSpy().and.returnValue('test-search')
+      })
+    };
+    mockStore = jasmine.createSpyObj(['select', 'dispatch']);
+
     TestBed.configureTestingModule({
-      declarations: [ProductListComponent]
+      declarations: [ProductListComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: Router, useValue: mockRouter },
+        { provide: Storage, useValue: mockStore },
+        Apollo,
+      ],
+      imports: [ApolloTestingModule],
     });
+
     fixture = TestBed.createComponent(ProductListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +46,55 @@ describe('ProductListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    it('should initialize the component', () => {
+
+      const mockQueryResult = { search: { items: [], totalItems: 0 } };
+      mockDataService.query.and.returnValue(of(mockQueryResult));
+      component['activeFacetValueIds$'] = of(['facet-1', 'facet-2']);
+      component['searchTerm$'] = of('test-search');
+
+      component.ngOnInit();
+
+      expect(component.categoryName).toEqual('test-slug');
+      expect(component.unfilteredTotalItems).toEqual(0);
+    });
+  });
+
+  describe('subscribeToFilters', () => {
+    it('should subscribe to filters', () => {
+      const mockFacetValues = ['facet-1', 'facet-2'];
+      const mockSearchTerm = 'test-search';
+      mockActivatedRoute.paramMap.pipe.and.returnValue(of({ get: jasmine.createSpy().and.returnValue(mockFacetValues) }));
+      mockActivatedRoute.queryParamMap.pipe.and.returnValue(of({ get: jasmine.createSpy().and.returnValue(mockSearchTerm) }));
+
+      component.subscribeToFilters();
+
+      expect(component['activeFacetValueIds']).toEqual(mockFacetValues);
+      expect(component['searchValue']).toEqual(mockSearchTerm);
+    });
+  });
+
+  describe('checkHistoryValues', () => {
+    it('should check history values', () => {
+      const mockFilter = { ids: ['facet-3'], search: 'new-search', category: 'new-category' };
+      component['isUrlChecked'] = false;
+      mockStore.select.and.returnValue(of([mockFilter]));
+
+      component.checkHistoryValues();
+
+      expect(mockRouter.navigate).toHaveBeenCalledWith(
+        ['./'],
+        {
+          queryParamsHandling: 'merge',
+          relativeTo: mockActivatedRoute,
+          state: {
+            noScroll: true,
+          },
+        }
+      );
+    });
   });
 });
